2101d13170978cfa81d4d42cc043861afcd4310d
Chris Blume <ProgramMax@gmail.com>
2015-06-20 19:57:52 -0700

    Accidentally had the Source Code Pro install script downloading the build files instead of the result files.

--- install-source-code-pro.sh
+++ install-source-code-pro.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir /tmp/adobefont
 cd /tmp/adobefont
-wget https://github.com/adobe-fonts/source-code-pro/archive/1.017.zip
-unzip 1.017.zip
+wget https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip
+unzip 1.017R.zip
 mkdir -p ~/.fonts
-cp source-code-pro-1.017/OTF/*.otf ~/.fonts
+cp source-code-pro-1.017R/OTF/*.otf ~/.fonts
 fc-cache -f -v
