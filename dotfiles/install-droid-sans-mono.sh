57cf7b5aa79e78dc1127a1ded9469d4ff56ef77d
Chris Blume <cblume@google.com>
2019-09-05 11:46:20 -0700

    Remove special fonts
    Previously, these dotfiles expected a special font with icon
    characters to be installed. This complicated things and wouldn't
    work if the user couldn't install fonts.
    
    Dependency on that font has been removed.
    
    This commit removes instructions for installing these these fonts
    as well as scripts to help with their installation.

--- dotfiles/install-droid-sans-mono.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir /tmp/adobefont
-cd /tmp/adobefont
-wget http://download.damieng.com/fonts/redistributed/DroidFamily.zip
-unzip DroidFamily.zip
-mkdir -p ~/.fonts
-cp DroidFonts/*.ttf ~/.fonts
-fc-cache -f -v
